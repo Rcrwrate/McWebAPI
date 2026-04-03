3cda4e544d6a3fbb093161cc79cfad2cf8174348
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2026-04-03 11:11:56 +0800

    daily

--- src/main/java/love/shirokasoke/webapi/Config.java
+++ src/main/java/love/shirokasoke/webapi/Config.java
@@ -8,10 +8,15 @@ public class Config {
 
     public static int httpPort = 40002;
     public static int nThreads = 10;
+
     public static boolean classDump = false;
+
     public static int itemThreadDelayMs = 10;
     public static int itemThreadBatchSize = 100;
     public static boolean itemThreadEnable = true;
+
+    public static String authToken = null;
+    public static String[] authUrlPrefixes = new String[] { "POST|setblock" };
 
     public static void synchronizeConfiguration(File configFile) {
         Configuration configuration = new Configuration(configFile);
@@ -20,23 +25,31 @@ public class Config {
             .getInt("httpPort", Configuration.CATEGORY_GENERAL, httpPort, 1024, 65535, "HTTP server port");
         nThreads = configuration.getInt("nThreads", Configuration.CATEGORY_GENERAL, 10, 4, 36, "WebServer Threads");
 
-        classDump = configuration.getBoolean("classDump", "debug", false, "allow class dump");
+        classDump = configuration.getBoolean("classDump", "debug", classDump, "allow class dump");
 
-        itemThreadEnable = configuration.getBoolean("enable", "itemThread", true, "enable Itemscache auto build");
+        itemThreadEnable = configuration
+            .getBoolean("enable", "itemThread", itemThreadEnable, "enable Itemscache auto build");
         itemThreadDelayMs = configuration.getInt(
             "DelayMs",
             "itemThread",
-            10,
+            itemThreadDelayMs,
             0,
             1000,
             "ItemsThread processing delay in milliseconds (0 to disable, higher = slower but less memory pressure)");
         itemThreadBatchSize = configuration.getInt(
             "BatchSize",
             "itemThread",
-            100,
+            itemThreadBatchSize,
             10,
             1000,
             "ItemsThread batch size for GC hints (items processed before System.gc() call)");
+
+        authToken = configuration.getString("authToken", "security", authToken, "simple use to auth HTTP request");
+        authUrlPrefixes = configuration.getStringList(
+            "authUrlPrefixes",
+            "security",
+            authUrlPrefixes,
+            "List of URL prefixes (e.g., 'POST|setblock') which need auth");
 
         if (configuration.hasChanged()) {
             configuration.save();
