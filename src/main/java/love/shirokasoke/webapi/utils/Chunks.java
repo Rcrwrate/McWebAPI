819a7dcc6995a26103aa8e8db601d42810859935
MoriaRuruka <3ZQHQJ6gHhY572rkreRqvA+MoriaRuruka@noreply.cnb.cool>
2026-04-08 17:20:25 +0800

    feat: 新增单个区块信息查询接口

--- src/main/java/love/shirokasoke/webapi/utils/Chunks.java
+++ src/main/java/love/shirokasoke/webapi/utils/Chunks.java
@@ -1,5 +1,7 @@
 package love.shirokasoke.webapi.utils;
 
+import java.util.List;
+
 import net.minecraft.world.chunk.Chunk;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,10 +13,14 @@ public class Chunks {
 
     private static final ObjectMapper mapper = Constant.mapper;
 
-    public static ObjectNode dump(Chunk chunk, ObjectNode data) {
+    public static ObjectNode dump(Chunk chunk, ObjectNode data, int dumpEntity) {
         ClassUtils.getClassInfo(chunk, data);
-        data.put("x", chunk.xPosition);
-        data.put("z", chunk.zPosition);
+        data.put("chunkX", chunk.xPosition);
+        data.put("chunkZ", chunk.zPosition);
+        data.put("xStart", chunk.xPosition << 4);
+        data.put("zStart", chunk.zPosition << 4);
+        data.put("xEnd", (chunk.xPosition << 4) + 15);
+        data.put("zEnd", (chunk.zPosition << 4) + 15);
         data.put("lastSaveTime", chunk.lastSaveTime);
 
         // 基础状态信息
@@ -27,15 +33,22 @@ public class Chunks {
 
         // 实体和方块实体数量
         data.put("tileEntityCount", chunk.chunkTileEntityMap != null ? chunk.chunkTileEntityMap.size() : 0);
-        int entityCount = 0;
-        if (chunk.entityLists != null) {
-            for (Object list : chunk.entityLists) {
-                if (list instanceof java.util.List) {
-                    entityCount += ((java.util.List<?>) list).size();
+        switch (dumpEntity) {
+            case 1:
+                int entityCount = 0;
+                if (chunk.entityLists != null) {
+                    for (Object list : chunk.entityLists) {
+                        if (list instanceof List) {
+                            entityCount += ((List<?>) list).size();
+                        }
+                    }
                 }
-            }
+                data.put("entityCount", entityCount);
+            case 0:
+                break;
+            default:
+                break;
         }
-        data.put("entityCount", entityCount);
 
         // 时间信息
         data.put("inhabitedTime", chunk.inhabitedTime);
@@ -44,6 +57,14 @@ public class Chunks {
     }
 
     public static ObjectNode dump(Chunk chunk) {
-        return dump(chunk, mapper.createObjectNode());
+        return dump(chunk, mapper.createObjectNode(), 1);
+    }
+
+    public static ObjectNode dump(Chunk chunk, ObjectNode data) {
+        return dump(chunk, data, 1);
+    }
+
+    public static ObjectNode dump(Chunk chunk, int dumpEntity) {
+        return dump(chunk, mapper.createObjectNode(), dumpEntity);
     }
 }
