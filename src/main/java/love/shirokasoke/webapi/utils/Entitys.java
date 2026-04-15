a1e29432ac0c362f6020173f19ccbc6c9168d33d
MoriaRuruka <3ZQHQJ6gHhY572rkreRqvA+MoriaRuruka@noreply.cnb.cool>
2026-04-15 17:19:52 +0800

    feat: 增加实体详细信息 wait: 攻击目标信息补全

--- src/main/java/love/shirokasoke/webapi/utils/Entitys.java
+++ src/main/java/love/shirokasoke/webapi/utils/Entitys.java
@@ -1,8 +1,15 @@
 package love.shirokasoke.webapi.utils;
 
+import java.util.Collection;
+
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import love.shirokasoke.webapi.Constant;
@@ -13,7 +20,10 @@ public class Entitys {
 
     public static ObjectNode dump(Object object, ObjectNode dataNode) {
         if (object instanceof Entity) {
-            dump((Entity) object, dataNode);
+            dumpEntity((Entity) object, dataNode);
+        }
+        if (object instanceof EntityLivingBase) {
+            dumpEntityLivingBase((EntityLivingBase) object, dataNode);
         }
         ClassUtils.getClassInfo(object, dataNode);
         return dataNode;
@@ -23,13 +33,14 @@ public class Entitys {
         return dump(object, mapper.createObjectNode());
     }
 
-    public static void dump(Entity object, ObjectNode dataNode) {
+    private static void dumpEntity(Entity object, ObjectNode root) {
+        ObjectNode dataNode = root.putObject("Entity");
         dataNode.put("name", object.getCommandSenderName());
         dataNode.put("entityId", object.getEntityId());
         dataNode.put(
-            "uniqueId",
-            object.getUniqueID()
-                .toString());
+                "uniqueId",
+                object.getUniqueID()
+                        .toString());
         dataNode.put("dimension", object.dimension);
 
         // 位置信息
@@ -88,9 +99,113 @@ public class Entitys {
         dataNode.put("chunkCoordZ", object.chunkCoordZ);
 
         // 骑行信息
-        dataNode.put("hasRidingEntity", object.ridingEntity != null);
-        dataNode.put("hasRiddenByEntity", object.riddenByEntity != null);
+        dataNode.put(
+                "RidingEntity",
+                object.ridingEntity != null ? object.ridingEntity.getUniqueID()
+                        .toString() : null);
+        dataNode.put(
+                "RiddenByEntity",
+                object.riddenByEntity != null ? object.riddenByEntity.getUniqueID()
+                        .toString() : null);
 
         NBT.dump(object.getEntityData(), dataNode);
+    }
+
+    private static void dumpEntityLivingBase(EntityLivingBase object, ObjectNode root) {
+        ObjectNode dataNode = root.putObject("EntityLivingBase");
+        // 生命值信息
+        dataNode.put("health", object.getHealth());
+        dataNode.put("maxHealth", object.getMaxHealth());
+        dataNode.put("absorptionAmount", object.getAbsorptionAmount());
+        dataNode.put("prevHealth", object.prevHealth);
+        dataNode.put("TotalArmor", object.getTotalArmorValue());
+        // 装备信息
+        ItemStack[] items = object.getLastActiveItems();
+        if (items != null) {
+            ArrayNode itemsNode = dataNode.putArray("items");
+            for (ItemStack i : items) {
+                itemsNode.add(Items.dump(i));
+            }
+        }
+
+        // 头部旋转信息
+        // dataNode.put("rotationYawHead", object.rotationYawHead);
+        // dataNode.put("prevRotationYawHead", object.prevRotationYawHead);
+        // dataNode.put("renderYawOffset", object.renderYawOffset);
+        // dataNode.put("prevRenderYawOffset", object.prevRenderYawOffset);
+
+        // 动画和动作状态
+        // dataNode.put("swingProgress", object.swingProgress);
+        // dataNode.put("prevSwingProgress", object.prevSwingProgress);
+        // dataNode.put("swingProgressInt", object.swingProgressInt);
+        // dataNode.put("isSwingInProgress", object.isSwingInProgress);
+        // dataNode.put("limbSwing", object.limbSwing);
+        // dataNode.put("limbSwingAmount", object.limbSwingAmount);
+        // dataNode.put("prevLimbSwingAmount", object.prevLimbSwingAmount);
+        // dataNode.put("moveStrafing", object.moveStrafing);
+        // dataNode.put("moveForward", object.moveForward);
+
+        // 受伤和攻击状态
+        dataNode.put("hurtTime", object.hurtTime);
+        dataNode.put("maxHurtTime", object.maxHurtTime);
+        dataNode.put("attackTime", object.attackTime);
+        dataNode.put("deathTime", object.deathTime);
+        dataNode.put("attackedAtYaw", object.attackedAtYaw);
+        dataNode.put("arrowCountInEntity", object.getArrowCountInEntity());
+        dataNode.put("arrowHitTimer", object.arrowHitTimer);
+
+        // 其他状态
+        dataNode.put("entityAge", object.getAge());
+        dataNode.put("jumpMovementFactor", object.jumpMovementFactor);
+        // dataNode.put("isPlayerSleeping", object.isPlayerSleeping());
+        dataNode.put("isOnLadder", object.isOnLadder());
+        dataNode.put("canBreatheUnderwater", object.canBreatheUnderwater());
+        dataNode.put("maxHurtResistantTime", object.maxHurtResistantTime);
+        dataNode.put("cameraPitch", object.cameraPitch);
+        dataNode.put("prevCameraPitch", object.prevCameraPitch);
+
+        // 属性信息
+        Collection<IAttributeInstance> att = object.getAttributeMap()
+                .getAllAttributes();
+        if (att != null) {
+            ObjectNode attributesNode = dataNode.putObject("attributes");
+            for (IAttributeInstance attr : att) {
+                String attrName = attr.getAttribute()
+                        .getAttributeUnlocalizedName();
+                ObjectNode attrNode = attributesNode.putObject(attrName);
+                attrNode.put("baseValue", attr.getBaseValue());
+                attrNode.put("currentValue", attr.getAttributeValue());
+            }
+        }
+
+        // 生物类型
+        dataNode.put(
+                "creatureAttribute",
+                object.getCreatureAttribute()
+                        .toString());
+
+        // 攻击目标信息
+        EntityLivingBase revengeTarget = object.getAITarget();
+        if (revengeTarget != null) {
+            dataNode.put("revengeTargetId", revengeTarget.getEntityId());
+        }
+
+        EntityLivingBase lastAttacker = object.getLastAttacker();
+        if (lastAttacker != null) {
+            dataNode.put("lastAttackerId", lastAttacker.getEntityId());
+        }
+
+        // 药水效果
+        Collection<PotionEffect> activeEffects = object.getActivePotionEffects();
+        if (activeEffects.size() != 0) {
+            ObjectNode potionsNode = dataNode.putObject("activePotionEffects");
+            for (PotionEffect effect : activeEffects) {
+                ObjectNode effectNode = potionsNode.putObject(String.valueOf(effect.getPotionID()));
+                effectNode.put("name", effect.getEffectName());
+                effectNode.put("duration", effect.getDuration());
+                effectNode.put("amplifier", effect.getAmplifier());
+                effectNode.put("isAmbient", effect.getIsAmbient());
+            }
+        }
     }
 }
