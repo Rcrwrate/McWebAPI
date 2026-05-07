03cf89fe2b6c181cb3d4cbd69cdd534d58c0efb6
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2026-05-07 16:42:42 +0800

    fix

--- after.sh
+++ after.sh
@@ -1,6 +1,7 @@
 bash CF.sh
 bash JetBrains.sh
 
+vsfix
 /root/.vscode-server/bin/$VSCODE_COMMIT_ID/bin/code-server --server-data-dir /root/.vscode-server --telemetry-level all \
     --install-extension vscjava.vscode-gradle \
     --install-extension vscjava.vscode-java-pack 
