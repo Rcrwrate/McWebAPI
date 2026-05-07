61c7bdea4cd2d31824df00f13b965efbde8708ab
MoriaRuruka <3ZQHQJ6gHhY572rkreRqvA+MoriaRuruka@noreply.cnb.cool>
2026-05-07 16:29:27 +0800

    fix after

--- after.sh
+++ after.sh
@@ -1,9 +1,7 @@
 bash CF.sh
 bash JetBrains.sh
 
-commit_id=$(ls /root/.vscode-server/bin)
-
-/root/.vscode-server/bin/$commit_id/bin/code-server --server-data-dir /root/.vscode-server --telemetry-level all \
+/root/.vscode-server/bin/$VSCODE_COMMIT_ID/bin/code-server --server-data-dir /root/.vscode-server --telemetry-level all \
     --install-extension vscjava.vscode-gradle \
     --install-extension vscjava.vscode-java-pack 
     # --install-extension georgewfraser.vscode-javac
