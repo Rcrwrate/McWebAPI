5ecd74b94e8f9bf67ba59bc8db9328d66138097b
MoriaRuruka <3ZQHQJ6gHhY572rkreRqvA+MoriaRuruka@noreply.cnb.cool>
2026-05-15 17:20:13 +0800

    实验性功能：地图 延迟较高

--- src/main/java/love/shirokasoke/webapi/Config.java
+++ src/main/java/love/shirokasoke/webapi/Config.java
@@ -17,6 +17,9 @@ public class Config {
     public static int itemThreadBatchSize = 100;
     public static boolean itemThreadEnable = true;
 
+    public static String BlockFile = "";
+    public static String BlockTileFolder = "";
+
     public static String authToken = "";
     public static String[] authUrlPrefixes = new String[] { "/setblock|GET|POST", "/chunk/force|GET|POST" };
     public static String[] langFiles = new String[] { "assets/minecraft/lang/zh_CN.lang" };
@@ -26,6 +29,11 @@ public class Config {
     public static int itemIconDelayMs = 10;
     public static int itemIconSize = 256;
     public static boolean itemDump = true;
+
+    public static boolean blockTileDumperEnable = false;
+    public static int blockTileDelayMs = 10;
+    public static int blockTileSize = 256;
+    public static boolean blockDump = true;
 
     public static void synchronizeConfiguration(File configFile) {
         Configuration configuration = new Configuration(configFile);
@@ -42,6 +50,13 @@ public class Config {
             "if ItemFile is set and valid, itemThread will be disable forcely");
         ItemIconFolder = configuration
             .getString("ItemIconFolder", "server", ItemIconFolder, "Client dumped Item Icon Folder");
+        BlockFile = configuration.getString(
+            "BlockFile",
+            "server",
+            BlockFile,
+            "if BlockFile is set and valid, block data will be loaded from this file");
+        BlockTileFolder = configuration
+            .getString("BlockTileFolder", "server", BlockTileFolder, "Client dumped Block Tile Folder");
         itemThreadEnable = configuration
             .getBoolean("enable", "server.itemThread", itemThreadEnable, "enable Itemscache auto build");
         itemThreadDelayMs = configuration.getInt(
@@ -97,6 +112,31 @@ public class Config {
             "client.item",
             itemDump,
             "Auto dump item's data to json if client.item.IconDumper is enable");
+
+        blockTileDumperEnable = configuration.getBoolean(
+            "enable",
+            "client.block.TileDumper",
+            blockTileDumperEnable,
+            "enable MapTileDumper on client load complete");
+        blockTileDelayMs = configuration.getInt(
+            "DelayMs",
+            "client.block.TileDumper",
+            blockTileDelayMs,
+            0,
+            1000,
+            "MapTileDumper delay between blocks in milliseconds");
+        blockTileSize = configuration.getInt(
+            "tileSize",
+            "client.block.TileDumper",
+            blockTileSize,
+            16,
+            256,
+            "MapTileDumper output tile size in pixels");
+        blockDump = configuration.getBoolean(
+            "blockDump",
+            "client.block",
+            blockDump,
+            "Auto dump block's data to json if client.block.TileDumper is enable");
 
         if (configuration.hasChanged()) {
             configuration.save();
