01ebe03566bdd139b7be805e6e87af27c0968583
MoriaRuruka <3ZQHQJ6gHhY572rkreRqvA+MoriaRuruka@noreply.cnb.cool>
2026-06-09 14:46:49 +0800

    nofix: Hodgepodge线程安全警告

--- sdk/ts/test/test.test.ts
+++ sdk/ts/test/test.test.ts
@@ -7,14 +7,16 @@ import * as v from "../src/validators";
 
 const api = new WebApiClient({ baseUrl: "http://localhost:40002" })
 
+const x = Math.floor(Math.random() * 40000) + 10000;
+const z = Math.floor(Math.random() * 40000) + 10000;
+const y = Math.floor(Math.random() * 50) + 50;
+
 describe("normal", () => {
     it("subitems", async () => {
-        const r = await api.getItem({ id: 4144 })
-        if (r.HasSubtypes) {
-            assert.ok(r.subs)
-            assert.ok(r.subs.length > 100)
-        }
-        console.log(v.ItemDetailSchema.validate(r))
-        assert.ok(v.ItemDetailSchema.validate(r).error == undefined)
+        const r = await api.loadChunk({ x, z, dim: 0, duration: 120 })
+
+        console.log(v.ChunkLoadResultSchema.validate(r))
+        console.log(`http://localhost:40002/block?x=${x}&y=${y}&z=${z}&dim=0`)
+
     })
 })
